f1c0029e35519856c9322a243d0b4b0d3437dd99
Xander Low <xander@xandydandy.com>
2023-04-10 13:25:53 -0700

    remove silly sketches

--- src/app/(p5)/sketches/blimbus/page.tsx
+++
@@ -1,9 +0,0 @@
-import Sketch from '@/components/client/sketches/blimbus'
-
-export const metadata = {
-  title: 'Blimbus'
-}
-
-const BlimbusPage = () => <Sketch />
-
-export default BlimbusPage
--- src/app/(p5)/sketches/krill-me/page.tsx
+++
@@ -1,9 +0,0 @@
-import Sketch from '@/components/client/sketches/krill-me'
-
-export const metadata = {
-  title: 'Krill Me'
-}
-
-const KrillMePage = () => <Sketch />
-
-export default KrillMePage
