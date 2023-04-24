7b6eaf032532c837f66e0276d84e02859975dde1
Xander Low <xander@xandydandy.com>
2023-04-23 22:31:44 -0700

    updated: noise map function

--- src/components/client/sketches/rachelle.tsx
+++ src/components/client/sketches/rachelle.tsx
@@ -28,25 +28,27 @@ const poem: string[] = [
 
 type Coordinates = [number, number, string]
 
+const noiseMap = (
+  p5: P5,
+  noiseValues: [number, number?, number?],
+  minMax: [number, number]
+) => p5.map(p5.noise(...noiseValues), 0, 1, minMax[0], minMax[1])
+
 const initialCoordinates = (
   p5: P5,
   poem: string[],
   margin: number
 ): Coordinates[] =>
   poem.map((string, i) => [
-    p5.map(
-      p5.noise(string.length * i, p5.width - margin),
-      0,
-      1,
-      margin,
-      p5.width - margin
+    noiseMap(
+      p5,
+      [string.length * i, p5.width - margin],
+      [margin, (p5.width - margin) * 0.5]
     ),
-    p5.map(
-      p5.noise(string.length * i, p5.height - margin * 2),
-      0,
-      1,
-      margin,
-      p5.height - margin
+    noiseMap(
+      p5,
+      [string.length * i, p5.height - margin * 2],
+      [margin, (p5.height - margin) * 0.5]
     ),
     string
   ])
@@ -54,15 +56,12 @@ const initialCoordinates = (
 const shape = (p5: P5, x: number, y: number, sMod = 1) => {
   const array = Array.from({ length: sMod })
   array.forEach((_, i) => {
-    // const w = i * sMod * 0.1
     const xPos = x + i * 10
     const yPos = y + i * 10
-    const w = p5.map(p5.noise(x * i, y * i), 0, 1, 10, 100)
+    const w = noiseMap(p5, [x * i, y * i], [10, 100])
     const h = i * sMod * 0.1
     p5.rect(xPos, yPos, w, h)
   })
-
-  p5.rect(x, y, p5.random(10) * sMod, p5.random(10) * sMod)
 }
 
 const Rachelle = () => {
