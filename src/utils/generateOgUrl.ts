2d951840c53e26277177cbf102b9966f140bb3b1
Xander Low <xander@xandydandy.com>
2023-07-02 14:23:39 -0700

    added: og image url function

---
+++ src/utils/generateOgUrl.ts
@@ -0,0 +1,24 @@
+import baseUrl from './baseUrl';
+
+type GenerateOgUrl = ({
+  title,
+  alt,
+  description,
+}: {
+  title?: string;
+  alt?: string;
+  description?: string;
+}) => string;
+
+export const generateOgUrl: GenerateOgUrl = ({ alt, description, title }) => {
+  `${baseUrl}/api/og-image.svg?title=${title}&description=${
+    description ?? title
+  }&alt=${title}`;
+  const url = new URL(`${baseUrl}/api/og-image.svg`);
+
+  url.searchParams.append('title', title ?? '');
+  url.searchParams.append('description', description ?? title ?? '');
+  url.searchParams.append('alt', alt ?? title ?? '');
+
+  return url.toString();
+};
