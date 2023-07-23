a39ff32216f6d6d7f410823bdb8a9ec114d3bc85
Xander Low <xander@xandydandy.com>
2023-07-23 15:26:04 -0700

    troubleshooting

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,6 +1,10 @@
-const { MODE, VERCEL_URL } = import.meta.env;
+const { MODE, VERCEL_URL, PRIVATE_BASE_URL } = import.meta.env
 
 const baseUrl =
-  MODE === 'development' ? 'http://localhost:3000' : `https://${VERCEL_URL}`;
+  MODE === 'production' && VERCEL_URL
+    ? `https://${VERCEL_URL}`
+    : MODE === 'production'
+    ? PRIVATE_BASE_URL
+    : 'http://localhost:3000'
 
-export default baseUrl;
+export default baseUrl
