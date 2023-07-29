2c89156e1ad243b4e86d294ea583d69555e6d949
Xander Low <xander@xandydandy.com>
2023-07-29 16:43:32 -0700

    updated: mdx components

--- src/components/MDXComponents.ts
+++ src/components/MDXComponents.ts
@@ -1,7 +1,9 @@
-import { H1, H2, H3 } from './Headings';
+import { H1, H2, H3 } from './Headings'
+import LI from './LI.astro'
 
 export const components = {
   h1: H1,
   h2: H2,
   h3: H3,
-};
+  li: LI
+}
