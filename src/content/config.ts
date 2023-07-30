e6ed1b2bd637899c016d4ecf135fc8f817efb00d
Xander Low <xander@xandydandy.com>
2023-07-29 18:33:07 -0700

    refactored: work experience collection from api endpoint

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,27 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from 'astro:content'
 
-const writing = defineCollection({
+const writingCollection = defineCollection({
+  type: 'content',
   schema: z.object({
     draft: z.boolean().default(false),
     title: z.string().nonempty(),
     date: z.date().transform(str => new Date(str)),
-    description: z.string().optional(),
-  }),
-});
+    description: z.string().optional()
+  })
+})
+
+const workHistoryCollection = defineCollection({
+  type: 'data',
+  schema: z.object({
+    role: z.string().nonempty(),
+    client: z.string().nonempty(),
+    startDate: z.string().transform(str => new Date(str)),
+    endDate: z.string().transform(str => new Date(str)),
+    responsibilities: z.string().array()
+  })
+})
 
 export const collections = {
-  writing,
-};
+  writing: writingCollection,
+  'work-history': workHistoryCollection
+}
