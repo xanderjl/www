a78e60a9851a0bbf4c341153462e9ee7abd62e45
Xander Low <xander@xandydandy.com>
2023-07-29 18:35:54 -0700

    lint fixes

--- src/theme/textStyles.ts
+++ src/theme/textStyles.ts
@@ -3,7 +3,7 @@ import { defineTextStyles } from '@pandacss/dev'
 const textStyles = defineTextStyles({
   body: {
     value: {
-      fontFamily: "'DM Mono', monospace",
+      fontFamily: '\'DM Mono\', monospace',
       fontWeight: 'normal',
       fontSize: 'md',
       lineHeight: 1.5,
