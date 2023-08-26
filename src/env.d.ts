3ccbabc43dad9cc0c026dbb70afd40bc56ea4d60
Xander Low <xander@xandydandy.com>
2023-08-25 17:56:51 -0700

    removed: link

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,22 +1,23 @@
+/* eslint-disable @typescript-eslint/triple-slash-reference */
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 
-import * as P5 from 'p5';
+import type * as P5 from 'p5'
 
 interface ImportMetaEnv {
-  readonly PRIVATE_BASE_URL: string;
-  readonly VERCEL_URL: string;
+  readonly PRIVATE_BASE_URL: string
+  readonly VERCEL_URL: string
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly env: ImportMetaEnv
 }
 
 declare global {
   interface Window {
-    p5: P5;
+    p5: P5
   }
 }
-  interface Window {
-    p5: P5;
-  }
+interface Window {
+  p5: P5
+}
