bc90c2d7d6700424ff2f025ac2d3a59341225837
Xander Low <xander@xandydandy.com>
2023-09-14 15:05:47 -0700

    added: new sketch

--- src/utils/p5/index.ts
+++ src/utils/p5/index.ts
@@ -1,4 +1,7 @@
+export * from './createGrid'
 export * from './setup'
 export * from './sketch'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+/* @ts-ignore */
 export * from './types'
 export * from './windowResized'
