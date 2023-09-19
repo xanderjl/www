4f620531f29e38c65896ac6e873bd2cd0d5737ea
Xander Low <xander@xandydandy.com>
2023-09-18 23:59:57 -0700

    added: resume pdf generator

--- src/pages/api/resume.ts
+++ src/pages/api/resume.ts
@@ -1,16 +1,34 @@
 import type { APIRoute } from 'astro'
+import { getCollection } from 'astro:content'
 import PDFDocument from 'pdfkit'
 
+import { formatDate } from '@/utils/formatDate'
+
+const collection = await getCollection('work-history')
+const experience = collection.sort(
+  (a, b) => b.data.startDate.getTime() - a.data.startDate.getTime()
+)
+
 export const GET: APIRoute = () => {
+  const contentDisposition = `inline; filename="xander-low-resume-${new Date().getFullYear()}.pdf"`
   const pdf = new PDFDocument({ size: 'A4', margin: 50 })
 
-  pdf.text('Hello World!')
+  experience.map(
+    ({ data: { client, endDate, responsibilities, role, startDate } }) => {
+      pdf.text(`${role} at ${client}`)
+      pdf.text(`${formatDate(startDate)} - ${formatDate(endDate)}`)
+
+      pdf.list(responsibilities)
+
+      pdf.moveDown()
+    }
+  )
   pdf.end()
 
-  return new Response(JSON.stringify(pdf), {
+  return new Response(pdf.read(), {
     headers: {
       'Content-Type': 'application/pdf',
-      'Content-Disposition': 'inline; filename="resume.pdf"'
+      'Content-Disposition': contentDisposition
     },
     status: 200,
     statusText: 'OK'
