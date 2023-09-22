4370bfd2d07cea5f958e684075979e4dd91e547f
Xander Low <xander@xandydandy.com>
2023-09-21 21:00:44 -0700

    removed: unused assets

--- src/pages/api/resume.ts
+++ src/pages/api/resume.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from 'astro'
 import { getCollection, getEntry } from 'astro:content'
+import { token } from 'styled-system/tokens'
 
 import { formatDate } from '@/utils/formatDate'
 import { PDF } from '@/utils/pdf'
 
-import DMMono from '../../../public/fonts/DM-Mono.ttf'
-
+// Source data
 const workHistory = await getCollection('work-history')
 const experience = workHistory.sort(
   (a, b) => b.data.startDate.getTime() - a.data.startDate.getTime()
@@ -13,6 +13,7 @@ const experience = workHistory.sort(
 const {
   data: { name, socials, education, email, phone, icons }
 } = await getEntry('resume-data', 'index')
+
 const contentDisposition = `inline; filename="xander-low-resume-${new Date().getFullYear()}.pdf"`
 
 // SVG and layout props
@@ -20,19 +21,23 @@ const margin = 36
 const iconSize = 24
 
 // Typography
-const h1 = 24
-const h2 = 18
+const h1 = 36
+const h2 = 24
+const h3 = 20
 const body = 12
+
+const highlightColor = token('colors.red.100')
 
 // Instantiate a new PDFDocument
 const doc = new PDF({
   size: 'A4',
-  margin
+  margin,
+  info: {
+    Title: `Xander Low Resume ${new Date().getFullYear()}`
+  }
 })
 
 export const GET: APIRoute = async () => {
-  doc.registerFont('DM Mono', DMMono)
-
   // Heading
   doc.fontSize(h1).text(name)
 
@@ -45,8 +50,9 @@ export const GET: APIRoute = async () => {
     .link(doc.x + iconSize, doc.y, iconSize, iconSize, socials['linkedin'].url)
     .addSVG(icons['linkedin'], doc.x + iconSize, doc.y)
 
-  // Contact info
+  doc.moveUp()
 
+  // Contact info
   // Phone
   doc.addSVG(
     icons['phone'],
@@ -55,7 +61,19 @@ export const GET: APIRoute = async () => {
   )
   doc
     .fontSize(body)
-    .text(phone, { link: `tel:${phone}`, underline: true, align: 'right' })
+    .highlight(
+      doc.page.width - margin - doc.widthOfString(email),
+      doc.y + doc.heightOfString(phone) * 0.5,
+      doc.widthOfString(email),
+      doc.heightOfString(phone) * 0.5,
+      { color: highlightColor }
+    )
+    .text(phone, {
+      link: `tel:${phone}`,
+      align: 'right'
+    })
+
+  doc.moveDown(0.25)
 
   // Email
   doc.addSVG(
@@ -63,20 +81,28 @@ export const GET: APIRoute = async () => {
     doc.page.width - margin * 5.5,
     doc.y - iconSize * 0.125
   )
-  doc.fontSize(body).text(email, {
-    link: `mailto:${email}`,
-    underline: true,
-    align: 'right'
-  })
+  doc
+    .fontSize(body)
+    .highlight(
+      doc.page.width - margin - doc.widthOfString(email),
+      doc.y + doc.heightOfString(email) * 0.5,
+      doc.widthOfString(email),
+      doc.heightOfString(email) * 0.5,
+      { color: highlightColor }
+    )
+    .text(email, {
+      link: `mailto:${email}`,
+      align: 'right'
+    })
 
-  doc.moveDown()
+  doc.moveDown(4)
 
   doc.fontSize(h2).text('Experience')
 
   experience.map(
     ({ data: { client, endDate, responsibilities, role, startDate } }) => {
       // Heading
-      doc.fontSize(h2).text(`${client} — ${role}`)
+      doc.fontSize(h3).text(`${client} — ${role}`)
 
       // Start and end date
       doc
@@ -86,7 +112,9 @@ export const GET: APIRoute = async () => {
       doc.moveDown()
 
       // Responsibilities
-      doc.fontSize(body).list(responsibilities, { bulletIndent: 20 })
+      doc
+        .fontSize(body)
+        .list(responsibilities, { bulletIndent: 10, bulletRadius: 2 })
 
       doc.moveDown(2)
     }
