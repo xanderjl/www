6e189946ac59f5379980ff6724eb444bdb921cc3
Xander Low <xander@xandydandy.com>
2023-09-29 22:59:59 -0700

    added: color tokens

---
+++ src/theme/tokens/colors.ts
@@ -0,0 +1,17 @@
+import { defineTokens } from "@pandacss/dev";
+
+export const colors = defineTokens.colors({
+  lavender: {
+    50: { value: "#f5f3ff" },
+    100: { value: "#e6e1ff" },
+    200: { value: "#ccc0ff" },
+    300: { value: "#b3a0ff" },
+    400: { value: "#9980ff" },
+    500: { value: "#7f60ff" },
+    600: { value: "#6650e6" },
+    700: { value: "#4c40bf" },
+    800: { value: "#332f99" },
+    900: { value: "#1a1f73" },
+    950: { value: "#0d0c39" },
+  },
+});
--- src/theme/tokens/index.ts
+++ src/theme/tokens/index.ts
@@ -1,5 +1,6 @@
 import { defineTokens } from "@pandacss/dev";
 
+import { colors } from "./colors";
 import { fonts } from "./fonts";
 
-export const tokens = defineTokens({ fonts });
+export const tokens = defineTokens({ colors, fonts });
