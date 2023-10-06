a6afa07397420978112cd1967dcc710857910f79
Xander Low <xander@xandydandy.com>
2023-10-06 13:36:05 -0700

    updated: astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import vercel from "@astrojs/vercel/serverless";
 import pandacss from "@pandacss/astro";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  integrations: [pandacss(), mdx(), solidJs()],
+  integrations: [
+    pandacss(),
+    mdx({ extendMarkdownConfig: true }),
+    solidJs()
+  ],
+  markdown: {
+    syntaxHighlight: "prism"
+  },
   output: "server",
 });
