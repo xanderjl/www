d29040c0757b1d975985a8b990ed30003a27de42
Xander Low <xander@xandydandy.com>
2023-10-10 14:19:16 -0700

    added: ecg data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
 import vercel from "@astrojs/vercel/serverless";
+import content from "@originjs/vite-plugin-content";
 import pandacss from "@pandacss/astro";
 import { defineConfig } from "astro/config";
 
@@ -15,4 +16,7 @@ export default defineConfig({
     syntaxHighlight: "prism"
   },
   output: "server",
+  vite: {
+    plugins: [content()]
+  }
 });
