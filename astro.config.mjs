06f72705021ba42192e9d4eeef0920d0f0628c37
Xander Low <xander@xandydandy.com>
2023-10-24 22:45:58 -0700

    fixed: redirects config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import vercel from "@astrojs/vercel/serverless";
 import pandacss from "@pandacss/astro";
 import { defineConfig } from "astro/config";
 import auth from "auth-astro";
+import { loadEnv } from "vite";
+
+const { BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,8 +26,8 @@ export default defineConfig({
   },
   output: "server",
   redirects: {
-    "/resume.pdf": {
-      destination: "/api/resume"
-    }
-  }
+    "/resume.pdf": "/api/resume"
+
+  },
+  site: BASE_URL
 });
