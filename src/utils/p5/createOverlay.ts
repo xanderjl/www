acbeb99a8d68ebb1b1e5032f1bd0a0a8f1d6644f
Xander Low <xander@xandydandy.com>
2023-10-29 20:37:54 -0700

    updated: geo output

--- src/utils/p5/createOverlay.ts
+++ src/utils/p5/createOverlay.ts
@@ -6,12 +6,12 @@ export const createOverlay = (
   p5: P5,
   background: ColorValue = [255, 253, 252],
   margin: number = p5.width * 0.1,
-): P5.Graphics => {
+): void => {
   const overlay = p5.createGraphics(p5.width, p5.height);
   overlay.background(background as unknown as P5.Color);
   overlay.erase();
   overlay.rect(margin, margin, p5.width - margin * 2, p5.height - margin * 2);
   overlay.noErase();
 
-  return overlay;
+  p5.image(overlay, 0, 0);
 };
