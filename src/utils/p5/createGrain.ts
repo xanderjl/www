cd8b40099f270a0e8f32211c7c06f5889b5b4bca
Xander Low <xander@xandydandy.com>
2023-10-30 21:42:30 -0700

    refactored: optimized utils

--- src/utils/p5/createGrain.ts
+++ src/utils/p5/createGrain.ts
@@ -1,6 +1,6 @@
 import type P5 from "p5";
 
-export const createGrain = (p5: P5, opacity = 10) => {
+export const createGrain = (p5: P5, opacity = 10): P5.Graphics => {
   const grain = p5.createGraphics(p5.width, p5.height);
   grain.loadPixels();
   Array.from({ length: p5.width }, (_, i) => {
@@ -10,5 +10,5 @@ export const createGrain = (p5: P5, opacity = 10) => {
   });
   grain.updatePixels();
 
-  p5.image(grain, 0, 0, p5.width, p5.height);
+  return grain;
 };
