715ee224d0d7a318970d7643cb9493ae0b7cdf3a
Xander Low <xander@xandydandy.com>
2023-11-02 13:04:19 -0700

    updated: colors

--- src/theme/tokens/colors.ts
+++ src/theme/tokens/colors.ts
@@ -1,6 +1,19 @@
 import { defineTokens } from "@pandacss/dev";
 
 export const colors = defineTokens.colors({
+  gray: {
+    100: { value: "#F5F5F4" },
+    200: { value: "#E7E5E4" },
+    300: { value: "#D6D3D1" },
+    400: { value: "#A8A29E" },
+    50: { value: "#FAFAF9" },
+    500: { value: "#78716C" },
+    600: { value: "#57534E" },
+    700: { value: "#44403C" },
+    800: { value: "#292524" },
+    900: { value: "#1C1917" },
+    950: { value: "#0D0C0A" },
+  },
   lavender: {
     100: { value: "#e6e1ff" },
     200: { value: "#ccc0ff" },
