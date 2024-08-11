15e057a2851bb02d016d049071b9197077c23fc8
Xander Low <xander@xandydandy.com>
2024-08-11 13:44:33 -0700

    semver bumps

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ export default defineConfig({
     enabled: true,
   },
   experimental: {
-    contentCollectionJsonSchema: true
+    contentCollectionCache: true,
   },
   integrations: [
     mdx({
